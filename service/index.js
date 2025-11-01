febcdb8ad4412b4304ba18ab585c05a3f188f289
esenecal <55216700+esenecal@users.noreply.github.com>
2025-11-01 10:13:20 -0600

    Using an example from the class, I put together a really basic backend api call with express and fetch, to be able to understand the concepts more.

--- service/index.js
+++ service/index.js
@@ -1,3 +1,30 @@
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+const express = require('express');
+const app = express();
+
 app.use(express.static('public'));
+
+const url = "https://api.github.com/users/octocat";
+
+app.get('/login', (req, res, next) => {
+    fetch(url)
+    .then((x) => {
+        console.log(typeof(x) + " " + x);       // x is first a response.
+        y = x.json();
+        console.log(typeof(y) + " " + y);       // Now it is a promise after calling .json
+        return y;                               // We return that promise, and the next .then will handle it.
+    })
+    .then((response) => {
+        console.log(typeof(response));          // Now this got the promise from y and turned it into an object.
+        console.log(response);          // This gets the json from the api and prints out the object in the vscode console.
+        res.send({ login: response.login });     // This line prints in response to a curl request in the console. (the actual call?)
+    });
+
+    
+});
+
+
+app.listen(port, function () {                          // Tells us which port we are listening on.
+    console.log(`Listening on port ${port}`);
+});
