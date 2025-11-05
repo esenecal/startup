2856246e79a4839c3d1a4e807143238cfdf2d1c3
esenecal <55216700+esenecal@users.noreply.github.com>
2025-11-05 15:31:47 -0700

    Added middleware and endpoints from login example code

--- service/index.js
+++ service/index.js
@@ -11,6 +11,17 @@ app.use(cookieParser());
 app.use(express.static('public'));
 app.use(express.json());
 
+app.post('/api/auth', async (req, res) => {
+  if (await getUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user);
+
+    res.send({ email: user.email });
+  }
+});
+
 // Contains the two potential api endpoint calls.
 const urls = ["https://thereportoftheweekapi.com/api/v1/reports/?category=Running%20On%20Empty", "https://thereportoftheweekapi.com/api/v1/reports/?category=Drink%20Review"];
 var apiRouter = express.Router();
@@ -41,6 +52,104 @@ let recipes_COLD = [{
 let recipes_BREAKFAST = [];
 let recipes_LUNCH = [];
 let recipes_DINNER = [];
+
+
+// ENDPOINTS FOR LOGIN. ---------------------------------------------------------------------------------
+
+// Login middleware
+app.use((req, res, next) => {
+  console.log("----------");
+  console.log(req.method);
+  console.log(req.originalUrl);
+  console.log(req.body);
+  console.log(users);
+  next();
+});
+
+// Create a user. Check to see if they exist, and if they do not, then create them.
+app.post('/api/auth', async (req, res) => {
+  if (await getUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user);
+
+    res.send({ email: user.email });
+  }
+});
+
+// logging in. Get the hashed password, compare it to provided password, then save authorization token in a cookie.
+app.put('/api/auth', async (req, res) => {
+  const user = await getUser('email', req.body.email);
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    setAuthCookie(res, user);
+
+    res.send({ email: user.email });
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// Logout.
+app.delete('/api/auth', async (req, res) => {
+  const token = req.cookies['token'];
+  const user = await getUser('token', token);
+  if (user) {
+    clearAuthCookie(res, user);
+  }
+
+  res.send({});
+});
+
+// Get.
+app.get('/api/user/me', async (req, res) => {
+  const token = req.cookies['token'];
+  const user = await getUser('token', token);
+  if (user) {
+    res.send({ email: user.email });
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+const users = [];
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+  };
+
+  users.push(user);
+
+  return user;
+}
+
+function getUser(field, value) {
+  if (value) {
+    return users.find((user) => user[field] === value);
+  }
+  return null;
+}
+
+function setAuthCookie(res, user) {
+  user.token = uuid.v4();
+
+  res.cookie('token', user.token, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+function clearAuthCookie(res, user) {
+  delete user.token;
+  res.clearCookie('token');
+}
+
+// -------------------------------------------------------------------------------------------------------
 
 // ENDPOINTS FOR RECIPE SENDING AND FETCHING. ------------------------------------------------------------
 
@@ -159,10 +268,6 @@ apiRouter.get('/randomFood', (req, res) => {
 });
 
 // ------------------------------------------------------------------------------------------------------------------
-
-
-// LOGIN ENDPOINTS
-
 
 
 
