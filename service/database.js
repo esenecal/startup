9d5f160d08cc2b096c6dd448b58600bff379d663
esenecal <55216700+esenecal@users.noreply.github.com>
2025-11-10 23:56:41 -0700

    Adding addRecipe and getRecipe. Working on writing them.

--- service/database.js
+++ service/database.js
@@ -13,6 +13,20 @@ const breakfastRecipes = db.collection("BREAKFAST");
 const lunchRecipes = db.collection("LUNCH");
 const dinnerRecipes = db.collection("DINNER");
 
+/*
+A recipe object:
+{
+    title: "title",
+    text: "recipe text",
+    tag: "tag type",
+}
+*/
+
+// to complete:
+// write out functions for adding and getting recipes
+// replace backend functions with database ones.
+// Set up user database
+
 // Test the connection.
 (async function testConnection() {
   try {
@@ -25,3 +39,31 @@ const dinnerRecipes = db.collection("DINNER");
     process.exit(1);
   }
 })();
+
+// Check to see if you should use await or return.
+// Add a recipe to the proper database
+async function addRecipe(recipe) {
+    switch(recipe.tag) {        // According to the value of the tag, place in the proper collection.
+        case "HOT":
+            await hotRecipes.insertOne(recipe);
+            break;
+        case "COLD":
+            await coldRecipes.insertOne(recipe);
+            break;
+        case "BREAKFAST":
+            await breakfastRecipes.insertOne(recipe);
+            break;
+        case "LUNCH":
+            await lunchRecipes.insertOne(recipe);
+            break;
+        case "DINNER":
+            await dinnerRecipes.insertOne(recipe);
+            break;
+        default:        // If there is no match, then say so.
+            console.log("NOT VALID TAG");
+    }
+}
+
+async function getRecipe() {
+
+}
