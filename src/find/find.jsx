78b1c4cdd7ef07cb242a9b3936f20291d6336421
esenecal <55216700+esenecal@users.noreply.github.com>
2025-12-01 16:58:56 -0700

    Mostly done with the first bit of the frontend function for displaying the user notification.

--- src/find/find.jsx
+++ src/find/find.jsx
@@ -246,7 +246,10 @@ function UserNotification({ webSocket }) {
     // Update on change.
     React.useEffect(() => {
         // We replace whatever the prior notification was with this new one.
+<<<<<<< HEAD
         console.log("Updated " + notification.username + " " + notification.tag);
+=======
+>>>>>>> d08c0d6 (Mostly done with the first bit of the frontend function for displaying the user notification.)
         webSocket.addObserver((notif) => {
             updateNotification(notif);
         });
@@ -254,4 +257,54 @@ function UserNotification({ webSocket }) {
 
     // Now for rendering this thing
     return <p id="user-notification" className="form-control border-3 border-success"> User {`${notification.username}`} just uploaded a {`${notification.tag}`} recipe!</p>;
+<<<<<<< HEAD
+=======
+}
+
+// Manage websocket. Send the "messages" which will be the username and the tag of the recipe uploaded.
+class notificationClient {
+    observers = [];     // Observers are objects with an event type, username of the person who submitted the recipe, and the tag.
+    connected = false;
+
+    constructor() {
+        // Adjust websocket protocol for connection protocol.
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        
+        this.socket.onopen = (event) => {
+            // We verify the connection by putting it into observers.
+            this.notifyObservers('system', 'websocket', 'connected');
+            this.connected = true;
+        };
+
+        // display
+        this.socket.onmessage = async (event) => {
+            const text = await event.data.text();
+            const notify = JSON.parse(text);
+            this.notifyObservers('received', notify.newUploadUser, notify.tag);
+        };
+
+        // close.
+        this.socket.onclose = (event) => {
+            this.notifyObservers('system', 'websocket', 'disconnected');
+            this.connected = false;
+        }
+
+    }
+
+    uploadNotification(newUploadUser, tag) {
+        this.notifyObservers('sent', newUploadUser, tag);
+        this.socket.send(JSON.stringify({ newUploadUser, tag }));
+    }
+
+    // Add an observer to the observer array.
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    // username for the name, tag for the recipe tag.
+    notifyObservers(event, username, tag) {
+        this.observers.forEach((h) => h({ event, username, tag }));
+    }
+>>>>>>> d08c0d6 (Mostly done with the first bit of the frontend function for displaying the user notification.)
 }
