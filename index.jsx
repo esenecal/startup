49decc349b4363d01aa61e2dbd4d857b6d2b80ac
esenecal <55216700+esenecal@users.noreply.github.com>
2025-12-02 23:59:44 -0700

    Finished code in find.jsx and passed NotifClient object into App

--- index.jsx
+++ index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from "./src/app";
+import { NotifClient } from './src/NotifClient'; 
 
 // This is essentially identifying the root component of index.html and rendering whatever is in App.jsx there.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App webSocket={new NotifClient()}/>);
--- src/NotifClient.js
+++ src/NotifClient.js
@@ -18,6 +18,22 @@ class NotifClient {
             const text = await event.data.text();
             const notif = JSON.parse(text);
             this.notifyObservers('received', notif.username, notif.tag);
-        }
+        };
+    }
+
+    // send notification over websocket.
+    sendNotification(username, tag) {
+        this.notifyObservers('sent', username, tag);
+        this.socket.send(JSON.stringify({ username, tag }));
+    }
+
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    notifyObservers(event, user, t) {
+        this.observers.forEach((h) => h({ event, user, t }));
     }
 }
+
+export { NotifClient }
