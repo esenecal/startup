05025aa37f860573aabf6c9b81ba37afc703ff8a
esenecal <55216700+esenecal@users.noreply.github.com>
2025-12-03 00:10:22 -0700

    Working on notifProxy

---
+++ service/notifProxy.js
@@ -0,0 +1,28 @@
+// File containing the backend for the websocket.
+
+const { WebSocketServer } = require('ws');
+
+function notifProxy(httpServer) {
+    // Websocket object
+    const socketServer = new WebSocketServer({ server: httpServer });
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        // Forward message to everyone except sender.
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach(function each(client) {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+
+        // Respond to pong messages by marking the connection alive.
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
+    });
+
+    // Periodically send a ping to ensure clients are alive.
+}
